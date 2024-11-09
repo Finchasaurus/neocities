9cebcb983777f296834331d5a334c5de9e9f4f27
Theo <finchasaurus@gmail.com>
2024-11-09 16:01:10 -0700

    tooltips

--- scripts/text-scroller.js
+++ scripts/text-scroller.js
@@ -2,11 +2,13 @@ const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
 document.querySelectorAll(".play-animation.text-scroll").forEach((element) => {
 	setInterval(() => {
-		element.innerText = element.innerText
-			.split("")
-			.map((c, i) => {
-				return letters[Math.floor(Math.random() * letters.length)];
-			})
-			.join("");
+		Array.from(element.childNodes).forEach((node) => {
+			if (node.nodeType === Node.TEXT_NODE) {
+				node.textContent = node.textContent
+					.split("")
+					.map(() => letters[Math.floor(Math.random() * letters.length)])
+					.join("");
+			}
+		});
 	}, 30);
 });
